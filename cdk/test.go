cbc8fc51c74dce1fca5c15a63657d6ebcb6e8d34
ryanolee <rizza@rizza.net>
2023-10-29 22:37:45 +0000

    Update CDK

--- cdk/test.go
+++ cdk/test.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"os"
+	"path"
+	"path/filepath"
 	"runtime"
 
 	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsec2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsecrassets"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsecs"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awslogs"
 
 	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
 	"github.com/aws/constructs-go/constructs/v10"
@@ -23,39 +28,77 @@ func NewPotStackStack(scope constructs.Construct, id string, props *TestStackPro
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	_, filename, _, _ := runtime.Caller(1)
+	_, filename, _, ok := runtime.Caller(1)
+	if !ok {
+		panic("unable to obtain filepath")
+	}
+
+	vpc := awsec2.NewVpc(stack, jsii.String("Vpc"), &awsec2.VpcProps{
+		IpAddresses: awsec2.IpAddresses_Cidr(jsii.String("172.31.0.0/24")),
+		MaxAzs: jsii.Number(2),
+		NatGateways: jsii.Number(0),
+		SubnetConfiguration: &[]*awsec2.SubnetConfiguration{
+			{
+				CidrMask: jsii.Number(26),
+				Name:     jsii.String("Public"),
+				SubnetType: awsec2.SubnetType_PUBLIC,
+			},
+		},
+	})
+
 	appImage := awsecrassets.NewDockerImageAsset(stack, jsii.String("MyAsset"), &awsecrassets.DockerImageAssetProps{
-		Directory: jsii.String("./../"),
+		Directory: jsii.String(path.Join(filepath.Dir(filename), "..")),
+		Exclude: jsii.Strings("./cdk"),
 	})
 
 	taskDefinition := awsecs.NewFargateTaskDefinition(stack, jsii.String("MyTask"), &awsecs.FargateTaskDefinitionProps{
 		Cpu:            jsii.Number(256),
 		MemoryLimitMiB: jsii.Number(512),
+		RuntimePlatform: &awsecs.RuntimePlatform{
+			CpuArchitecture: awsecs.CpuArchitecture_X86_64(),
+			OperatingSystemFamily: awsecs.OperatingSystemFamily_LINUX(),
+		},
+	})
+
+	logGroup := awslogs.NewLogGroup(stack, jsii.String("LogGroup"), &awslogs.LogGroupProps{
+		LogGroupName: jsii.String("/ryan-pot/nodes"),
+		Retention:    awslogs.RetentionDays_ONE_WEEK,
 	})
 
 	container := taskDefinition.AddContainer(jsii.String("TaskDefinition"), &awsecs.ContainerDefinitionOptions{
 		Image:     awsecs.ContainerImage_FromDockerImageAsset(appImage),
 		Essential: jsii.Bool(true),
+		Logging: awsecs.NewAwsLogDriver(&awsecs.AwsLogDriverProps{
+			LogGroup: logGroup,
+			StreamPrefix: jsii.String("/ryan-pot"),
+		}),
+		
 		PortMappings: &[]*awsecs.PortMapping{
 			{
 				ContainerPort: jsii.Number(8080),
-				HostPort:      jsii.Number(80),
 			},
 		},
 	})
 
 	taskDefinition.SetDefaultContainer(container)
 
-	cluster := awsecs.NewCluster(stack, jsii.String("EcsCluster"), &awsecs.ClusterProps{})
+	cluster := awsecs.NewCluster(stack, jsii.String("EcsCluster"), &awsecs.ClusterProps{
+		Vpc: vpc,
+	})
 	awsecs.NewFargateService(stack, jsii.String("EcsService"), &awsecs.FargateServiceProps{
 		Cluster:           cluster,
+		CapacityProviderStrategies: &[]*awsecs.CapacityProviderStrategy{
+			{
+				CapacityProvider: jsii.String("FARGATE_SPOT"),
+				Weight:           jsii.Number(1),
+			},
+		},
+		VpcSubnets: 	  &awsec2.SubnetSelection{SubnetType: awsec2.SubnetType_PUBLIC},
 		TaskDefinition:    taskDefinition,
 		DesiredCount:      jsii.Number(1),
 		AssignPublicIp:    jsii.Bool(true),
 		MaxHealthyPercent: jsii.Number(200),
 		MinHealthyPercent: jsii.Number(0),
-
-		HealthCheckGracePeriod: awscdk.Duration_Seconds(jsii.Number(60)),
 	})
 
 	return stack
@@ -78,26 +121,8 @@ func main() {
 // env determines the AWS environment (account+region) in which our stack is to
 // be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
 func env() *awscdk.Environment {
-	// If unspecified, this stack will be "environment-agnostic".
-	// Account/Region-dependent features and context lookups will not work, but a
-	// single synthesized template can be deployed anywhere.
-	//---------------------------------------------------------------------------
-	return nil
-
-	// Uncomment if you know exactly what account and region you want to deploy
-	// the stack to. This is the recommendation for production stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String("123456789012"),
-	//  Region:  jsii.String("us-east-1"),
-	// }
-
-	// Uncomment to specialize this stack for the AWS Account and Region that are
-	// implied by the current CLI configuration. This is recommended for dev
-	// stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
-	//  Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
-	// }
+	return &awscdk.Environment{
+		Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
+		Region:  jsii.String("eu-west-1"),
+	   }
 }
